7f0e5cd88dc33ecca4ca5331ea0a71d9801af747
ichaves <ichaves@cys.com.ar>
2018-09-11 16:59:40 -0300

    Cambios falopas by Leandro Baldassarre

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # PracticaProfesionalSupervisada
+
+
+BACKLOG
+
+11/09/2018
+Ya esta la navegabilidad en la mayoría de las páginas y la migración al español.
+Restantes
+
+1. Accesos a:
+		 customer-order.html
+		 customer-orders.html
+		 customer-account.html
+
+2. Footer
+
+3. Lógicas del servidor.
+
+4. Esquema lógico de base de datos.
+
+5. Google Maps.
+
+6. Cupones de descuento (newsletter)
+
+7. Pantalla Finalizar Pedido (Desarrollo)
--- js/gmaps.init.js
+++ js/gmaps.init.js
@@ -11,8 +11,13 @@ function map() {
     var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
 	el: '#map',
+<<<<<<< HEAD
+	lat: -34.662387,
+	lng: -58.364848,
+=======
 	lat: -12.043333,
 	lng: -77.028333,
+>>>>>>> 53a811adbe3d1b014295fc6237d0ee057be70872
 	zoomControl: true,
 	zoomControlOpt: {
 	    style: 'SMALL',
@@ -30,8 +35,13 @@ function map() {
     var image = 'img/marker.png';
 
     map.addMarker({
+<<<<<<< HEAD
+	lat: -34.662387,
+	lng: -58.364848,
+=======
 	lat: -12.043333,
 	lng: -77.028333,
+>>>>>>> 53a811adbe3d1b014295fc6237d0ee057be70872
 	icon: image/* ,
 	 title: '',
 	 infoWindow: {
